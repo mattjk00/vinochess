78f71ad9e6c77eb3dbefe46d2417bb13d250e4eb
Matthew K <mattjk@outlook.com>
2020-11-26 02:08:44 -0500

    Functionality for basic piece moving added. Started building a state system. Notes.txt added with more notes.

--- app/board.js
+++ app/board.js
@@ -1,4 +1,4 @@
-import GRIDSIZE, { PIECESIZE } from './pieces.js'
+import {GRIDSIZE, PIECESIZE } from './pieces.js'
 //const GRIDSIZE = Pieces.GRIDSIZE;
 
 export default class Board {
@@ -12,35 +12,53 @@ export default class Board {
      * @returns {Number} The array index that it was added at.
      */
     addPiece(piece) {
-        let arrayPos = GRIDSIZE * piece.y + piece.x;
+        let arrayPos = Math.floor(GRIDSIZE * piece.y + piece.x);
+        piece.gridIndex = arrayPos;
         this.grid[arrayPos] = piece;
 
         return arrayPos;
     }
 
     /**
+     * Finds the data for the piece in the grid based on a given sprite reference.
+     * @param {Sprite} sprite 
+     * @returns The found grid piece or -1 if none was found.
+     */
+    dataFromSprite(sprite) {
+        for (let i = 0; i < this.grid.length; i++) {
+            // find the piece that has a reference to the given sprite
+            if (Object.is(this.grid[i].sprite, sprite)) {
+                return this.grid[i];
+            }
+        }
+        return -1;
+    }
+
+    /**
      * Tries to move a piece on the board
      * @param {Number} pi - The piece index in the grid.
-     * @param {Number} mi - The move index that the piece will perform.
+     * @param {Number} nx - The new x
+     * @param {Number} ny - The new y
+     * @param {Number} m - The change in the grid index position
      */
-    movePiece(pi, mi) {
+    movePiece(pi, nx, ny, m) {
         // Get the piece and the move for the piece
         let piece = this.grid[pi];
-        let move = piece.moves[mi];
+        let move = m;
         
         // TODO: Verify Move
 
         // Move the piece's data in both dimensions
-        piece.x += move.dx;
-        piece.y += move.dy;
-        piece.gridIndex = pi + move.m;
+        piece.x = nx;
+        piece.y = ny;
+        piece.gridIndex = pi + m;
 
         // Move the piece's sprite to reflect the move
         piece.sprite.x = piece.x * PIECESIZE;
         piece.sprite.y = piece.y * PIECESIZE;
 
         // Update the grid with the move
-        this.grid[pi + move.m] = piece;
+        this.grid[pi + m] = piece;
         this.grid[pi] = 0;
     }
 }
--- app/main.js
+++ app/main.js
@@ -1,15 +1,20 @@
 // Imports
 import Board from './board.js';
-import { newPawn, PIECESIZE } from './pieces.js';
+import { newPawn, PIECESIZE, GRIDSIZE } from './pieces.js';
 
 // Constants and Aliases
 const IMGPATH = "app/imgs/";
 const Loader = PIXI.loader;
+const IDLE = 0;
+const MOVECHOICE = 1;
 
 // Pixi App Configuration
 let app = new PIXI.Application({width: 800, height: 800});
 app.renderer.autoResize = true;
 document.body.appendChild(app.view);
+
+let gameState = IDLE;
+let pieceSelected;
 
 // Load textures
 Loader
@@ -24,12 +29,55 @@ let board = new Board();
  */
 function setup() {
     boardSprite = createSpriteAndAdd("board.png");
+    boardSprite.interactive = true;
+    boardSprite.on('mousedown', boardClicked);
+    boardSprite.on('touchstart', boardClicked);
 
     let ps = addPawn(2, 2, 1);
     
-    board.movePiece(ps.gridIndex, 0);
+    //board.movePiece(ps.gridIndex, 0, 0, 0);
 
     app.ticker.add(delta => gameLoop(delta));
+}
+
+/**
+ * Called when a game piece is clicked.
+ * @param {EventData} e 
+ */
+function pieceClicked(e) {
+    // if in idle state before click
+    if (gameState == IDLE) {
+        // get the piece data for the sprite that was clicked
+        pieceSelected = board.dataFromSprite(e.target);
+        // if the data was found
+        if (pieceSelected != -1) {
+            // enter moving selection state.
+            gameState = MOVECHOICE;
+        }
+    }
+    
+}
+
+/**
+ * Is called when the game board is clicked.
+ * @param {EventData} e 
+ */
+function boardClicked(e) {
+    // convert the mouse coordinates to 8x8 grid coordinates.
+    let gridX = Math.floor(e.data.global.x/80);
+    let gridY = Math.floor(e.data.global.y/80);
+    // if the user is in moving state
+    if (gameState == MOVECHOICE) {
+        // find the difference in the clicked place to move and the current piece's position
+        let moveX = gridX - pieceSelected.x;
+        let moveY = gridY - pieceSelected.y;
+        // convert the 2D move to a 1D move
+        let move = (moveY * GRIDSIZE + moveX);
+        // move the selected piece
+        board.movePiece(pieceSelected.gridIndex, gridX, gridY, move);
+        // return to idle gamestate
+        gameState = IDLE;
+    }
 }
 
 /**
@@ -43,6 +91,7 @@ function createSpriteAndAdd(tname) {
     let sprite = new PIXI.Sprite(
         Loader.resources[IMGPATH + tname].texture
     );
+    
     // add sprite to the stage
     app.stage.addChild(sprite);
     return sprite;
@@ -62,6 +111,9 @@ function addPawn(x, y, owner) {
     let pawnData = newPawn("pawn", x, y, owner);
     // create a sprite to represent the piece
     let pawnSprite = createSpriteAndAdd("piece1.png");
+    pawnSprite.interactive = true;
+    pawnSprite.on('mousedown', pieceClicked);
+    pawnSprite.on('touchstart', pieceClicked);
     
     // Move the sprite to the correct position
     pawnSprite.x = x * PIECESIZE;
